167571ff5ca160a6526a158d381b124d115181e1
Alex-WalkDone <alex.kronin777@gmail.com>
2019-12-13 21:22:47 +0300

    Update Wall.ino
    Добавил константы для wallie

--- Wall/Wall.ino
+++ Wall/Wall.ino
@@ -1,9 +1,29 @@
+//количество кнопок и диодов
+const int BTN_COUNT = 20;
+
+//массив с кнопками
+int btn[BTN_COUNT]; // Присвоить номера пинов!!!!
+//массив с диодами
+int led[BTN_COUNT]; // Присвоить номера пинов!!!!
+//массив с масками (какие должы быть состояния кнопок во время игры)
+boolean mask[BTN_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+//счётчик
+unsigned long currentTime = 0;
+
+
 void setup() {
-  // put your setup code here, to run once:
-//стас-унитаз
+  //выбираем режим работы для пинов диодов и кнопок
+  for (int i = 0; i < BTN_COUNT; i++) {
+    pinMode(btn[i], INPUT_PULLUP);
+    pinMode(led[i], OUTPUT);
+  }
+  //последоательный порт для отладки
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
