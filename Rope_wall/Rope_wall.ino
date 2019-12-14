87f10f3008e9b468c278cb5c434dbb584c76f67b
Alex-WalkDone <alex.kronin777@gmail.com>
2019-12-14 22:31:17 +0300

    Update Rope_wall.ino

--- Rope_wall/Rope_wall.ino
+++ Rope_wall/Rope_wall.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
-const byte LED_PIN 5
-const byte NUM_LEDS 60
+const byte LED_PIN = 5;
+const byte NUM_LEDS =  60;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 byte score1 = 0; //кол-во нажатий первой стороной
 byte score2 = 0; //кол-во нажатий второй стороной
@@ -8,8 +8,8 @@ byte int1 = 2; //прерывание 1 группы кнопок
 byte int2 = 3; //прерывание 2 группы кнопок
 
 //задаем номера пинов для кнопок и их значения по умолчанию
-byte btnPins[BTN_COUNT] = {49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30}; //номера пинов
-byte btnState[BTN_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; //начальные состояния кнопок
+byte btnPins[20] = {49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30}; //номера пинов
+byte btnState[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; //начальные состояния кнопок
 
 bool state1 = 0;    //флаг нажатий первых 10 кнопок
 bool state2 = 0;    //флаг нажатий вторых 10 кнопок
@@ -17,6 +17,7 @@ bool state2 = 0;    //флаг нажатий вторых 10 кнопок
 //=======================================================================================
 //прерывание 1
 void BUTTON1() {
+   // Serial.println('1');
   //деактивируем прерывания
   detachInterrupt(digitalPinToInterrupt(int1));
   detachInterrupt(digitalPinToInterrupt(int2));
@@ -30,15 +31,20 @@ void BUTTON1() {
   //в цикле суммируем все элементы массива состояний
   //Если хотя бы один элемент из первых 10 равен 1, то state1 равен 1
   for (byte i = 0; i < 10; i++) {
-    state1 = state1 + ButtonState[i];
+    state1 = state1 + btnState[i];
   }
 
   //активируем прерывания
   attachInterrupt(digitalPinToInterrupt (int1), BUTTON1, RISING);
   attachInterrupt(digitalPinToInterrupt (int2), BUTTON2, RISING);
+
+    for (byte i = 0; i < 10; i++) {
+      Serial.println(btnState [i]);
+  }
 }
 
 void BUTTON2() {
+  Serial.println('2');
   //деактивируем прерывания
   detachInterrupt(digitalPinToInterrupt(int1));
   detachInterrupt(digitalPinToInterrupt(int2));
@@ -52,12 +58,17 @@ void BUTTON2() {
   //в цикле суммируем все элементы массива состояний
   //Если хотя бы один элемент из вторых 10 равен 1, то state2 равен 1
   for (byte i = 10; i < 20; i++) {
-    state2 = state2 + ButtonState[i];
+    state2 = state2 + btnState[i];
   }
 
   //активируем прерывания
   attachInterrupt(digitalPinToInterrupt (int1), BUTTON1, RISING);
   attachInterrupt(digitalPinToInterrupt (int2), BUTTON2, RISING);
+
+      for (byte i = 10; i < 20; i++) {
+      Serial.print(btnState [i]);
+           Serial.println();
+  }
 }
 
 void setup() {
@@ -105,7 +116,6 @@ void kanat(byte R, byte G, byte B, byte int1, byte int2, byte brightness) {
   {
     strip.setPixelColor(i, strip.Color(R, G, B));
   }
-  strip.show();
 
   //включение справа в соответствии с текущим значением score2
   for (byte i = 31; i < 31 + score2; i++)
@@ -130,7 +140,7 @@ void kanat(byte R, byte G, byte B, byte int1, byte int2, byte brightness) {
 
     //обнуляем состояние первых 10 кнопок
     for (byte i = 0; i < 10; i++) {
-      ButtonState[i] = 0;
+      btnState[i] = 0;
     }
 
     //если количество нажатий 1 игрока достигло 29, то...
@@ -139,6 +149,7 @@ void kanat(byte R, byte G, byte B, byte int1, byte int2, byte brightness) {
     }
     //обнулить флаг нажатий
     state1 = 0;
+        state2 = 0;
   }
 
   //если есть нажатие, то...
@@ -157,7 +168,7 @@ void kanat(byte R, byte G, byte B, byte int1, byte int2, byte brightness) {
 
     //обнуляем состояние вторых 10 кнопок
     for (byte i = 10; i < 20; i++) {
-      ButtonState[i] = 0;
+      btnState[i] = 0;
     }
 
     //если количество нажатий 2 игрока достигло 29, то...
@@ -165,6 +176,7 @@ void kanat(byte R, byte G, byte B, byte int1, byte int2, byte brightness) {
       winKanat(2, 0, 0, 255); //игрок 2 выиграл
     }
     //обнулить флаг нажатий
+        state1 = 0;
     state2 = 0;
   }
 }
@@ -174,7 +186,7 @@ void kanat(byte R, byte G, byte B, byte int1, byte int2, byte brightness) {
 
 //=======================================================================================
 //функция цвета светодиодов (R, G, B 0...255) для победы игрока под номером N.
-void winKanat(N, R, G, B) {
+void winKanat(byte N,byte R,byte G, byte B) {
 
   //winPlay(); //воспроизвести звук выигрыша
   //matrixWriteText(String WIN, int N);  //выиграл игрок N
