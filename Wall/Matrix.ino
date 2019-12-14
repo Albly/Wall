945ce40ef14c7aca87aa2fe3ea05a83a5f97a588
Alex-WalkDone <alex.kronin777@gmail.com>
2019-12-14 04:09:39 +0300

    Update Matrix.ino

--- Wall/Matrix.ino
+++ Wall/Matrix.ino
@@ -1,18 +1,42 @@
-void displayScore(int left, int right) {
-  // выводит счет на матрицу
-  //drawChar. drawChar( x, y, символ, цвет, фон, размер );
-  if (left > 9) {
-    matrix.drawChar(0, 0, (left / 10), HIGH, LOW, 1);
-    matrix.drawChar(8, 0, (left % 10), HIGH, LOW, 1);
+void matrixWriteScore(int left, int right) {
+  String left_str = String(left);
+  String right_str = String(right);
+
+  char left_ch[2];
+  char right_ch[2];
+  left_str.toCharArray(left_ch, 2);
+  right_str.toCharArray(right_ch, 2);
+
+
+  if (left < 10) {
+    matrix.drawChar(2, 0, '0', HIGH, LOW, 1);
+    matrix.drawChar(8, 0, left_ch[0], HIGH, LOW, 1);
   }
   else {
-    matrix.drawChar(0, 0, left, HIGH, LOW, 1);
+    String(left / 10).toCharArray(left_ch, 2);
+    matrix.drawChar(2, 0, left_ch[0], HIGH, LOW, 1);
+    String(left % 10).toCharArray(left_ch, 2);
+    matrix.drawChar(8, 0, left_ch[0], HIGH, LOW, 1);
   }
-  if (right > 9) {
-    matrix.drawChar(16, 0, (right / 10), HIGH, LOW, 1);
-    matrix.drawChar(24, 0, (right % 10), HIGH, LOW, 1);
+
+  if (right < 10) {
+    matrix.drawChar(19, 0, '0', HIGH, LOW, 1);
+    matrix.drawChar(25, 0, right_ch[0], HIGH, LOW, 1);
   }
   else {
-    matrix.drawChar(16, 0, right, HIGH, LOW, 1);
+    String(right / 10).toCharArray(left_ch, 2);
+    matrix.drawChar(19, 0, right_ch[0], HIGH, LOW, 1);
+    String(right / 10).toCharArray(right_ch, 2);
+    matrix.drawChar(25, 0, right_ch[0], HIGH, LOW, 1);
   }
+  matrix.drawPixel(15, 1, HIGH);
+  matrix.drawPixel(16, 1, HIGH);
+  matrix.drawPixel(15, 2, HIGH);
+  matrix.drawPixel(16, 2, HIGH);
+
+  matrix.drawPixel(15, 5, HIGH);
+  matrix.drawPixel(16, 5, HIGH);
+  matrix.drawPixel(15, 6, HIGH);
+  matrix.drawPixel(16, 6, HIGH);
+  matrix.write();
 }
